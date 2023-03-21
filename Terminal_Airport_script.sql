6fe955241e256fffce0d818bbacc75f60886fde0
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-03-21 18:30:33 -0400

    Initial clean up script and terminal & airport table existence check

---
+++ Terminal_Airport_script.sql
@@ -0,0 +1,41 @@
+--CLEANUP SCRIPT
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+show user;
+--CLEANUP SCRIPT
+set serveroutput on
+/*
+The Below block of code checks if the tables TERMINAL and AIRPORT
+are present in the database. If present the tables are dropped and 
+a message indicating the success is the output. In the case of any errors
+a custom error message is displayed.
+*/
+declare
+    v_table_exists varchar(1) := 'Y';
+    v_sql varchar(2000);
+begin
+   dbms_output.put_line('Start schema cleanup');
+   for i in (select 'TERMINAL' table_name from dual union all
+             select 'AIRPORT' table_name from dual
+   )
+   loop
+   dbms_output.put_line('....Drop table '||i.table_name);
+   begin
+       select 'Y' into v_table_exists
+       from USER_TABLES
+       where TABLE_NAME=i.table_name;
+
+       v_sql := 'drop table '||i.table_name;
+       execute immediate v_sql;
+       dbms_output.put_line('........Table '||i.table_name||' dropped successfully');
+       
+   exception
+       when no_data_found then
+           dbms_output.put_line('........Table already dropped');
+   end;
+   end loop;
+   dbms_output.put_line('Schema cleanup successfully completed');
+exception
+   when others then
+      dbms_output.put_line('Failed to execute code:'||sqlerrm);
+end;
+/
