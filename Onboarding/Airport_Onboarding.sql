516ee9b1c69f25c9b26f4e0385a02885d049b071
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-04-07 18:31:02 -0400

    Aiport onboarding package done

--- Onboarding/Airport_Onboarding.sql
+++ Onboarding/Airport_Onboarding.sql
@@ -24,7 +24,41 @@ PROCEDURE insert_airport(
     END IF;  
     
     -- Regex validations
+    
+    -- Validate airport name input
+    IF REGEXP_LIKE(a_airport_name, '/^[a-zA-Z ]*$/') = FALSE THEN
+      RAISE_APPLICATION_ERROR(-20002, 'Only alphabets and spaces are allowed');
+    END IF;
+    
+    -- Validate city input
+    IF REGEXP_LIKE(a_city, '/^[a-zA-Z ]*$/') = FALSE THEN
+      RAISE_APPLICATION_ERROR(-20003, 'Only alphabets and spaces are allowed');
+    END IF;
+    
+    -- Validate state input
+    IF REGEXP_LIKE(a_state, '/^[a-zA-Z ]*$/') = FALSE THEN
+      RAISE_APPLICATION_ERROR(-20004, 'Only alphabets and spaces are allowed');
+    END IF;
+    
+    -- Validate country input
+    IF REGEXP_LIKE(a_country, '/^[a-zA-Z ]*$/') = FALSE THEN
+      RAISE_APPLICATION_ERROR(-20005, 'Only alphabets and spaces are allowed');
+    END IF;
 
+    INSERT INTO PASSENGER (
+      airport_id,
+      airport_name,
+      city,
+      state,
+      country
+    ) VALUES (
+      admin.airport_seq.nextval,
+      a_airport_name,
+      a_city,
+      a_state,
+      a_country
+    );
+    
   END insert_airport;
 
 END airport_pkg;
