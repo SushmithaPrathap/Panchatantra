be033d93edea9ebfd18be9914387f157f031d601
Sushmitha Prathap <prathap.s@northeastern.edu>
2023-04-07 20:23:35 -0400

    bug fix in run.sql

--- Airline_Staff/Run.sql
+++ Airline_Staff/Run.sql
@@ -29,26 +29,6 @@ BEGIN
     p_no_pax => 300,
     p_airline_id => 1010,
     p_seats_filled => 0,
-    p_terminal_id => 1
+    p_terminal_id => 6001
   );
 END;
-
-
--- select * from passenger;
--- select * from orders;
---update orders set status = 'Success' where order_id = 10;
-
--- BEGIN
---   PASSENGER_TICKET_PACKAGE.INSERT_PASSENGER(
---     p_passenger_id => 10,
---     p_age => 22,
---     p_address => '789 Pine St, Anytown, USA',
---     p_sex => 'Female',
---     p_govt_id_nos => 'VWX234',
---     p_first_name => 'Rose',
---     p_last_name => 'Pink',
---     p_dob =>  TO_DATE('2000-03-25', 'YYYY-MM-DD'),
---     p_contact_number => 8901234567,
---     p_email => 'avabrown@example.com'
---   );
--- END; 
