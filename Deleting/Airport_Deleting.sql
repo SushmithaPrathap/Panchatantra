53cbc3d429f2f0bbbe6cfc8ac5f0a92db51143b7
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-04-07 21:54:15 -0400

    Airport delete package body changes

--- Deleting/Airport_Deleting.sql
+++ Deleting/Airport_Deleting.sql
@@ -12,3 +12,31 @@ CREATE OR REPLACE PACKAGE passenger_deleting_pkg AS
   );
 END passenger_deleting_pkg;
 /
+
+CREATE OR REPLACE PACKAGE BODY passenger_deleting_pkg AS
+
+    PROCEDURE delete_airport(
+    a_airport_id       IN NUMBER,
+    a_airport_name     IN VARCHAR2,
+    a_city             IN VARCHAR2,
+    a_state            IN VARCHAR2,
+    a_country          IN VARCHAR2
+    ) IS
+    BEGIN
+    
+      DELETE FROM AIRPORT
+      WHERE airport_id = a_airport_id;
+      COMMIT;
+    
+      DBMS_OUTPUT.PUT_LINE('Airport deleted successfully');  
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('Airport with airport_id ' || a_airport_id || ' not found.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('Error deleting airport with airport_id ' || a_airport_id || ': ' || SQLERRM);
+            
+    END delete_airport;
+END passenger_deleting_pkg;
+/
+
+SHOW ERRORS;
