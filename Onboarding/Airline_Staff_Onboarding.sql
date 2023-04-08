4fdf09b446466d9e9d220359fe5db77751abdf97
shankarabhi <shankar.abhi@northeastern.edu>
2023-04-08 18:28:57 -0400

    Bug Fixes

--- Onboarding/Airline_Staff_Onboarding.sql
+++ Onboarding/Airline_Staff_Onboarding.sql
@@ -3,8 +3,11 @@ This Package is used for inserting airline_staff data
 After a airline_staff signs up an Order gets generated in the order table
 When a airline_staff deetes a ticket the number of airline_staffs on a flight reduces
 */
+show user;
+select airline_staff_seq.currval from dual;
+
 CREATE OR REPLACE PACKAGE airline_staff_pkg AS  
-  FUNCTION check_airline_id_exists()in_airline_id in NUMBER) RETURN NUMBER;
+  FUNCTION check_airline_id_exists(in_airline_id in NUMBER) RETURN NUMBER;
   PROCEDURE insert_airline_staff(
     in_staff_id              IN NUMBER,
     in_airline_id         IN NUMBER,
@@ -21,21 +24,21 @@ END airline_staff_pkg;
 /
 
 CREATE OR REPLACE PACKAGE BODY airline_staff_pkg AS
-FUNCTION check_airline_id_exists(p_airline_id NUMBER)
+FUNCTION check_airline_id_exists(in_airline_id NUMBER)
 RETURN NUMBER
 AS
   v_airline_count NUMBER;
 BEGIN
   SELECT COUNT(*) INTO v_airline_count
   FROM AIRLINES
-  WHERE AIRLINE_ID = p_airline_id;
+  WHERE AIRLINE_ID = in_airline_id;
 
   IF v_airline_count > 0 THEN
-    RETURN p_airline_id;
+    RETURN in_airline_id;
   ELSE
     RETURN NULL;
   END IF;
-END;
+END check_airline_id_exists;
 
 PROCEDURE insert_airline_staff(
     in_staff_id              IN NUMBER,
@@ -50,12 +53,13 @@ PROCEDURE insert_airline_staff(
     in_gender in VARCHAR2
   ) IS
   INVALID_INPUTS EXCEPTION;
+  airline_id_exists NUMBER;
   BEGIN
     IF in_staff_id IS NULL OR in_airline_id IS NULL OR in_gender IS NULL OR in_first_name IS NULL OR in_last_name  IS NULL OR in_address IS NULL OR in_ssn IS NULL OR in_email_id IS NULL OR in_contact_number IS NULL OR in_job_group IS NULL THEN
       RAISE INVALID_INPUTS;
     END IF;  
-     airline_id_exists := airline_staff_pkg.check_airline_id_exists(in_airline_id);
-      IF airline_id_exists IS NULL
+      airline_id_exists := check_airline_id_exists(in_airline_id);
+      IF airline_id_exists IS NULL then
         RAISE_APPLICATION_ERROR(-20002, 'airline ID does not exist');
       END IF;
      -- Validate gender
@@ -67,21 +71,18 @@ PROCEDURE insert_airline_staff(
       RAISE_APPLICATION_ERROR(-20002, 'Incorrect job group');
     END IF;
     -- Validate SSN input
-    IF REGEXP_LIKE(in_ssn, '^((?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\d{2})\d{3} (?!00)\d{2} (?!0{4})\d{4})|((?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4})$') = FALSE THEN
+   IF REGEXP_LIKE(in_ssn, '^((?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\d{2})\d{3} (?!00)\d{2} (?!0{4})\d{4})|((?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4})$') = FALSE THEN
       RAISE_APPLICATION_ERROR(-20006, 'Invalid SSN format');
-
-    END IF;
+   END IF;
      -- Validate first name input
     IF REGEXP_LIKE(in_first_name, '/^[a-zA-Z]+$/') = FALSE THEN
-      RAISE_APPLICATION_ERROR(-20006, 'Invalid first name');
+        RAISE_APPLICATION_ERROR(-20006, 'Invalid first name');
+    END IF;
       -- Validate last name input
-    END IF;
     IF REGEXP_LIKE(in_last_name, '/^[a-zA-Z]+$/') = FALSE THEN
-      RAISE_APPLICATION_ERROR(-20006, 'Invalid last name');
+        RAISE_APPLICATION_ERROR(-20006, 'Invalid last name');
     END IF;
 
-
-  
     -- Validate contact_number input
     IF LENGTH(in_contact_number) != 10 THEN
       RAISE_APPLICATION_ERROR(-20004, 'Contact Number must be a 10-digit value');
@@ -91,7 +92,7 @@ PROCEDURE insert_airline_staff(
       RAISE_APPLICATION_ERROR(-20004, 'Address must be in the format ex., 23 XYZ, Boston, MA 02115');
     END IF;
     -- Validate email input
-    IF REGEXP_LIKE(in_email_id _email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') = FALSE THEN
+    IF REGEXP_LIKE(in_email_id, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') = FALSE THEN
       RAISE_APPLICATION_ERROR(-20006, 'Invalid email format');
     END IF;
     INSERT INTO airline_staff (
