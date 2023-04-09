d5baf3a0218ace2af76f063665f01b4128b10ab1
harshitaranganath27 <ranganath.h@northeastern.edu>
2023-04-08 20:18:02 -0400

    bug fix

--- Onboarding/Airline_Staff_Onboarding.sql
+++ Onboarding/Airline_Staff_Onboarding.sql
@@ -2,13 +2,10 @@
 This Package is used for onboarding airline_staff data
 Airline staff is onboarded once airport and airline is added. Each staff has a unique ID.
 */
-show user;
-select airline_staff_seq.currval from dual;
 
 CREATE OR REPLACE PACKAGE airline_staff_pkg AS  
   FUNCTION check_airline_id_exists(in_airline_id in NUMBER) RETURN NUMBER;
   PROCEDURE insert_airline_staff(
-    in_staff_id              IN NUMBER,
     in_airline_id         IN NUMBER,
     in_first_name             IN VARCHAR2,
     in_last_name     IN VARCHAR2,
@@ -40,7 +37,6 @@ BEGIN
 END check_airline_id_exists;
 
 PROCEDURE insert_airline_staff(
-    in_staff_id              IN NUMBER,
     in_airline_id         IN NUMBER,
     in_first_name             IN VARCHAR2,
     in_last_name     IN VARCHAR2,
@@ -54,46 +50,55 @@ PROCEDURE insert_airline_staff(
   INVALID_INPUTS EXCEPTION;
   airline_id_exists NUMBER;
   BEGIN
-    IF in_staff_id IS NULL OR in_airline_id IS NULL OR in_gender IS NULL OR in_first_name IS NULL OR in_last_name  IS NULL OR in_address IS NULL OR in_ssn IS NULL OR in_email_id IS NULL OR in_contact_number IS NULL OR in_job_group IS NULL THEN
+    IF in_airline_id IS NULL OR in_gender IS NULL OR in_first_name IS NULL OR in_last_name  IS NULL OR in_address IS NULL OR in_ssn IS NULL OR in_email_id IS NULL OR in_contact_number IS NULL OR in_job_group IS NULL THEN
       RAISE INVALID_INPUTS;
-    END IF;  
+    END IF;
     --validate if airline ID exists before pushing into airline staff table
       airline_id_exists := check_airline_id_exists(in_airline_id);
       IF airline_id_exists IS NULL then
-        DBMS_OUTPUT.PUT_LINE('airline ID does not exist');
+        DBMS_OUTPUT.PUT_LINE('Airline ID does not exist');
+        RETURN;
       END IF;
      -- Validate gender
     IF in_gender NOT IN ('Male', 'Female', 'Other') THEN
       DBMS_OUTPUT.PUT_LINE('Sex must be specified as male, female, or other');
+      RETURN;
     END IF;
      -- Validate job group
     IF in_job_group NOT IN (1,2,3,4,5) THEN
       DBMS_OUTPUT.PUT_LINE('Incorrect job group');
+      RETURN;
     END IF;
     -- Validate SSN input
    IF REGEXP_LIKE(in_ssn, '^((?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\d{2})\d{3} (?!00)\d{2} (?!0{4})\d{4})|((?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4})$') = FALSE THEN
       DBMS_OUTPUT.PUT_LINE('Invalid SSN format');
+      RETURN;
    END IF;
      -- Validate first name input
     IF REGEXP_LIKE(in_first_name, '/^[a-zA-Z]+$/') = FALSE THEN
         DBMS_OUTPUT.PUT_LINE('Invalid first name');
+        RETURN;
     END IF;
       -- Validate last name input
     IF REGEXP_LIKE(in_last_name, '/^[a-zA-Z]+$/') = FALSE THEN
         DBMS_OUTPUT.PUT_LINE('Invalid last name');
+        RETURN;
     END IF;
 
     -- Validate contact_number input
     IF LENGTH(in_contact_number) != 10 THEN
       DBMS_OUTPUT.PUT_LINE('Contact Number must be a 10-digit value');
+      RETURN;
     END IF;
     -- Validate address input
     IF REGEXP_LIKE(in_address, '^[0-9]{1,5} [a-zA-Z0-9\s]{1,50}, [a-zA-Z\s]{2,50}, [A-Z]{2} [0-9]{5}$') = FALSE THEN
       DBMS_OUTPUT.PUT_LINE('Address must be in the format ex., 23 XYZ, Boston, MA 02115');
+      RETURN;
     END IF;
     -- Validate email input
     IF REGEXP_LIKE(in_email_id, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') = FALSE THEN
       DBMS_OUTPUT.PUT_LINE('Invalid email format');
+      RETURN;
     END IF;
     INSERT INTO airline_staff (
      staff_id,
@@ -118,7 +123,12 @@ PROCEDURE insert_airline_staff(
     in_job_group,
     in_gender
     );
-    
+   COMMIT;
+   EXCEPTION
+      DBMS_OUTPUT.PUT_LINE('An error occurred while inserting data');
+      rollback;
+      RETURN;
+      
   END insert_airline_staff;
 
 END airline_staff_pkg;
--- Updating/AirlineStaff_Update.sql
+++ Updating/AirlineStaff_Update.sql
@@ -1,5 +1,5 @@
 /*
-This Package is used for inserting and updating airline staff data
+This Package is used for updating airline staff data
 */
 
 CREATE OR REPLACE PACKAGE airline_staff_updating_pkg AS
@@ -20,46 +20,88 @@ END airline_staff_updating_pkg;
 
 CREATE OR REPLACE PACKAGE BODY airline_staff_updating_pkg AS
 PROCEDURE update_airline_staff(
-    p_passenger_id     IN NUMBER ,
-    p_age              IN NUMBER,
-    p_address          IN VARCHAR2,
-    p_sex              IN VARCHAR2,
-    p_govt_id_nos      IN VARCHAR2,
-    p_first_name       IN VARCHAR2,
-    p_last_name        IN VARCHAR2,
-    p_dob              IN DATE,
-    p_contact_number   IN VARCHAR2,
-    p_email            IN VARCHAR2
+    in_staff_id              IN NUMBER,
+    in_airline_id         IN NUMBER,
+    in_first_name             IN VARCHAR2,
+    in_last_name     IN VARCHAR2,
+    in_address      IN VARCHAR2,
+    in_ssn      IN VARCHAR2,
+    in_email_id             IN VARCHAR2,
+    in_contact_number   IN NUMBER,
+    in_job_group          IN VARCHAR2,
+    in_gender in VARCHAR2
   ) IS
     v_passenger_id NUMBER;
     v_count NUMBER;    
   BEGIN
-    IF p_passenger_id IS NULL THEN
-      -- Prompt user to enter passenger ID
-      dbms_output.put('Please enter the passenger ID: ');
-      Return;
-    ELSIF p_passenger_id IS NULL or p_age IS NULL OR p_address IS NULL OR p_sex IS NULL OR p_govt_id_nos IS NULL OR p_first_name IS NULL OR p_last_name IS NULL OR p_dob IS NULL OR p_contact_number IS NULL OR p_email IS NULL THEN
-      dbms_output.put('All input parameters must be specified');
-      Return;      
-    ELSIF p_sex NOT IN ('Male', 'Female', 'Other') THEN
-      dbms_output.put('Sex must be specified as male, female, or other');
-      Return;      
-    -- Validate gov_id_nos input
-    ELSIF LENGTH(p_govt_id_nos) != 10 THEN
-       dbms_output.put('Govt ID Number must be a 10-digit value');
-      Return;            
-    -- Validate contact_number input
-    ELSIF LENGTH(p_contact_number) != 10 THEN
-      dbms_output.put('Contact Number must be a 10-digit value');
-      Return;         
-    -- Validate email input
-    ELSIF REGEXP_LIKE(p_email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') = FALSE THEN
-      dbms_output.put('Invalid email format');
-      Return;         
-    ELSE
-      v_passenger_id := p_passenger_id;
+    INVALID_INPUTS EXCEPTION;
+  airline_id_exists NUMBER;
+  BEGIN
+    IF in_staff_id IS NULL OR in_airline_id IS NULL OR in_gender IS NULL OR in_first_name IS NULL OR in_last_name  IS NULL OR in_address IS NULL OR in_ssn IS NULL OR in_email_id IS NULL OR in_contact_number IS NULL OR in_job_group IS NULL THEN
+      RAISE INVALID_INPUTS;
+    END IF;  
+    --validate if airline ID exists before pushing into airline staff table
+      airline_id_exists := check_airline_id_exists(in_airline_id);
+      IF airline_id_exists IS NULL then
+        DBMS_OUTPUT.PUT_LINE('airline ID does not exist');
+      END IF;
+     -- Validate gender
+    IF in_gender NOT IN ('Male', 'Female', 'Other') THEN
+      DBMS_OUTPUT.PUT_LINE('Sex must be specified as male, female, or other');
+    END IF;
+     -- Validate job group
+    IF in_job_group NOT IN (1,2,3,4,5) THEN
+      DBMS_OUTPUT.PUT_LINE('Incorrect job group');
+    END IF;
+    -- Validate SSN input
+   IF REGEXP_LIKE(in_ssn, '^((?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\d{2})\d{3} (?!00)\d{2} (?!0{4})\d{4})|((?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4})$') = FALSE THEN
+      DBMS_OUTPUT.PUT_LINE('Invalid SSN format');
+   END IF;
+     -- Validate first name input
+    IF REGEXP_LIKE(in_first_name, '/^[a-zA-Z]+$/') = FALSE THEN
+        DBMS_OUTPUT.PUT_LINE('Invalid first name');
+    END IF;
+      -- Validate last name input
+    IF REGEXP_LIKE(in_last_name, '/^[a-zA-Z]+$/') = FALSE THEN
+        DBMS_OUTPUT.PUT_LINE('Invalid last name');
     END IF;
 
+    -- Validate contact_number input
+    IF LENGTH(in_contact_number) != 10 THEN
+      DBMS_OUTPUT.PUT_LINE('Contact Number must be a 10-digit value');
+    END IF;
+    -- Validate address input
+    IF REGEXP_LIKE(in_address, '^[0-9]{1,5} [a-zA-Z0-9\s]{1,50}, [a-zA-Z\s]{2,50}, [A-Z]{2} [0-9]{5}$') = FALSE THEN
+      DBMS_OUTPUT.PUT_LINE('Address must be in the format ex., 23 XYZ, Boston, MA 02115');
+    END IF;
+    -- Validate email input
+    IF REGEXP_LIKE(in_email_id, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') = FALSE THEN
+      DBMS_OUTPUT.PUT_LINE('Invalid email format');
+    END IF;
+    INSERT INTO airline_staff (
+     staff_id,
+    airline_id,
+    first_name,
+    last_name,
+    address,
+    ssn,
+    email_id,
+    contact_number,
+    job_group,
+    gender
+    ) VALUES (
+    ADMIN.airline_staff_seq.NEXTVAL,
+    in_airline_id,
+    in_first_name,
+    in_last_name,
+    in_address,
+    in_ssn,
+    in_email_id,
+    in_contact_number,
+    in_job_group,
+    in_gender
+    );
+  
     SELECT COUNT(*) INTO v_count FROM PASSENGER WHERE passenger_id = v_passenger_id;
     
     IF v_count = 0 THEN
@@ -67,13 +109,6 @@ PROCEDURE update_airline_staff(
       RETURN;
     END IF;
 
-    -- Check if email already exists
-    SELECT COUNT(*) INTO v_count FROM PASSENGER WHERE email = p_email AND passenger_id <> p_passenger_id;
-    
-    IF v_count > 0 THEN
-      DBMS_OUTPUT.PUT_LINE('Email already in use by another passenger');
-      RETURN;
-    END IF;
 
     BEGIN
       UPDATE PASSENGER
@@ -90,14 +125,13 @@ PROCEDURE update_airline_staff(
       WHERE
         passenger_id = v_passenger_id;
       commit;  
-      DBMS_OUTPUT.PUT_LINE('Data Successfully Updated BLAHAHHAHAHA');
+      DBMS_OUTPUT.PUT_LINE('Data Successfully Updated');
     EXCEPTION
       WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No passenger found with the given ID');
+        DBMS_OUTPUT.PUT_LINE('No airline staff found with the given ID');
         RETURN;
       WHEN OTHERS THEN
-        --DBMS_OUTPUT.PUT_LINE('An error occurred while updating the passenger');
-        DBMS_OUTPUT.PUT_LINE('An error occurred while updating the passenger: ' || SQLERRM);      
+        DBMS_OUTPUT.PUT_LINE('An error occurred while updating airline staff: ' || SQLERRM);      
         RETURN;
     END;
 
