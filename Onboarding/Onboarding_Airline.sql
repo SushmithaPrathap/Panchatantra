ff74e39ecd98b2bf0944bad488c957d458b81681
jainanv <jain.anv@northeastern.edu>
2023-04-11 19:15:04 -0400

    validations and test cases

--- Onboarding/Onboarding_Airline.sql
+++ Onboarding/Onboarding_Airline.sql
@@ -16,6 +16,7 @@ CREATE OR REPLACE PACKAGE BODY airline_pkg AS
     v_airline_name IN VARCHAR2
   )
   IS
+  v_count INTEGER;
   BEGIN
     IF v_airline_code IS NULL OR v_airline_name IS NULL THEN
       --RAISE_APPLICATION_ERROR(-20001, 'All input parameters must be specified');
@@ -25,9 +26,18 @@ CREATE OR REPLACE PACKAGE BODY airline_pkg AS
       DBMS_OUTPUT.PUT_LINE('Invalid airline code format');
       --RAISE_APPLICATION_ERROR(-20006, 'Invalid airline code format');
     END IF;
+    SELECT COUNT(*) INTO v_count FROM airlines WHERE airline_code = v_airline_code;
+    IF v_count > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Airline code already exists');
+    RETURN;
+    END IF;
     INSERT INTO airlines (airline_id, route_number, airline_code, airline_name)
     VALUES (admin.airline_seq.nextval, admin.airline_route_sequence.nextval, v_airline_code, v_airline_name);
     COMMIT;
+    EXCEPTION
+    WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error occured while inserting airline');
+    rollback;
   END;
 
 END airline_pkg;
--- Onboarding/test_onboarding_airline.sql
+++ Onboarding/test_onboarding_airline.sql
@@ -5,9 +5,15 @@ EXECUTE airline_pkg.insert_airline('6E', 'Indigo');
 EXECUTE airline_pkg.insert_airline('AI', 'Air India');
 EXECUTE airline_pkg.insert_airline(null, 'Air Asia');
 EXECUTE airline_pkg.insert_airline('khj', 'Air Asia');
+EXECUTE airline_pkg.insert_airline('6E', 'jjr');
+EXECUTE airline_pkg.insert_airline('EK', 'jjr');
+EXECUTE airline_pkg.insert_airline('SJ', 'Spicejet');
+EXECUTE airline_pkg.insert_airline('Sk', '');
+EXECUTE airline_pkg.insert_airline('SP', ' ');
 select * from airlines;
-
+select * from passenger;
 --Testing update airline
+EXECUTE update_airline_pkg.update_airline_name(NULL, 'British Airways');
 EXECUTE update_airline_pkg.update_airline_name(NULL, 'WD6');
 --Testing delete airline
-EXECUTE delete_airline_pkg.delete_airline(1002);
+EXECUTE delete_airline_pkg.delete_airline(13);
