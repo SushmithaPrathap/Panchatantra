65fc3ffcefc7a4e3a1c953e4fc140b39f88c0967
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-04-12 15:04:20 -0400

    Changes

--- ExecutionScripts/AnalystScripts.sql
+++ ExecutionScripts/AnalystScripts.sql
@@ -123,8 +123,21 @@ SELECT * FROM flight_cancellation_counts;
 
 
 /*
-View 4 : Baggage transaction – The number of bags per transaction
-
-SELECT COUNT(B.baggage_id) AS BAGGAGE_COUNT, O.ORDER_ID FROM ((BAGGAGE B JOIN TICKET T ON B.TICKET_ID = T.TICKET_ID) JOIN ORDERS O ON O.ORDER_ID = T.ORDER_ID) GROUP BY O.ORDER_ID;
-
+View 6 : Baggage transaction – The number of bags per transaction
 */
+
+BEGIN
+  EXECUTE IMMEDIATE 'CREATE OR REPLACE VIEW baggage_count_per_order AS
+    SELECT COUNT(B.baggage_id) AS BAGGAGE_COUNT, O.ORDER_ID 
+    FROM ((BAGGAGE B JOIN TICKET T ON B.TICKET_ID = T.TICKET_ID) 
+    JOIN ORDERS O ON O.ORDER_ID = T.ORDER_ID) 
+    GROUP BY O.ORDER_ID';
+    DBMS_OUTPUT.PUT_LINE('baggage_count_per_order view was created successfully');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+END;
+/
+
+-- Test case for View 6
+select * from baggage_count_per_order;
